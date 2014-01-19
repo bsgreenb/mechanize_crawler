066fbefb98646d5a5cf10e73d05d7467613d74b5
Ben Greenberg <bsgreenb@gmail.com>
2014-01-19 12:59:22 -0800

    new rails project for this scraper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MechanizeCrawler::Application.config.secret_key_base = 'bdcd4251f15c56edbf6022221fe23414dd80fa6811297e9dbaa76f15fcd5f13f73b175d1446deb89c3c50019e916d9b0c39c1f0a7e03e19622a2c4bf2a7d0357'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MechanizeCrawler::Application.config.session_store :cookie_store, key: '_mechanize_crawler_session'
