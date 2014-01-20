0704749ee4dbad0f0f55dc2acc4af608723d75c4
Ben Greenberg <bsgreenb@gmail.com>
2014-01-19 16:24:30 -0800

    got the initial stuff

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 gem 'mechanize'
 gem 'domainatrix'
 gem 'fakeweb'
--- test/models/mechanize_crawler_test.rb
+++ test/models/mechanize_crawler_test.rb
@@ -1,12 +1,31 @@
 require "test/unit"
 require 'mechanize'
+require 'domainatrix'
 
 #TODO: move this from here so that it initializes seperately.
 class Mechanize::Page
   def get_assets
     # Begin by getting the css ...
+    css = []
+    self.search('link[@href]').each do |link_tag| # loop through <link> tags with an href attribute
+      link_url = (self.uri.merge link_tag[:href]).to_s
+      next unless link_url.match(URI.regexp(['http', 'https'])) #needs to mathch uri regex
+      next unless link_url.match(/.+\.css$/)
+
+      css << link_url
+    end
+    css.uniq!
 
     # Then get the JS ...
+    js = []
+    self.search('script[@src]').each do |script_tag| # loop through <script> tags with a src attribute
+      script_url = (self.uri.merge script_tag[:src]).to_s
+      next unless script_url.match(URI.regexp(['http', 'https']))
+      js << script_url
+    end
+    js.uniq!
+
+    return {css: css, js: js}
 
   end
 
@@ -14,20 +33,35 @@ class Mechanize::Page
     #We loop through all the links and return an array of unique absolute hrefs
     #Gets a unique list of intra-domain links from the page
 
-    #TODO: only use valid URI's.  check on regex
-    #TODO: only grab the ones on the domain
-    #TODO: uniq the links
-
-    #TODO: don't count anchor stuff as part of the list.
-    #TODO: exclude self from the list
-
     links = []
 
     self.links.each do |link|
-      links << (self.uri.merge link.uri).to_s if link.uri
+      link = link.uri
+
+      # Make sure it has a valid link url...
+      next if !link #needs a uri
+
+
+      # get rid of the anchor fragment, it could cause us trouble..
+      link.fragment = nil
+
+      if link.host #If it has a host it's more than just a path url
+        # Make sure it's on the same top-level domain
+
+        domainatrix = Domainatrix.parse(link.host)
+        next unless domainatrix && (domainatrix.domain + '.' + domainatrix.public_suffix) == 'joingrouper.com'
+      end
+
+      link_url = (self.uri.merge link).to_s
+
+      next unless link_url.match(URI.regexp(['http', 'https'])) #needs to mathch uri regex
+
+      next if link_url == self.uri.to_s # Don't consider self linking a thing.
+
+      links << link_url
     end
 
-    links.uniq!
+    return links.uniq
 
   end
 
@@ -48,12 +82,14 @@ class MechanizeCrawlerTest < Test::Unit::TestCase
     # Do nothing
   end
 
+
   def test_get_assets
     expected_assets = {
         css:
           ['https://www.joingrouper.com/assets/application-82224c93c060bf8eb8bf1bb2bfd1eee5.css'],
         js:
             [
+              'https://cdn.optimizely.com/js/279869788.js',
               'https://use.typekit.com/ilp6fts.js',
               'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js',
               'https://www.joingrouper.com/assets/application-08a1ec4e9dd3cd46ce88383742eb3a75.js'
@@ -62,7 +98,9 @@ class MechanizeCrawlerTest < Test::Unit::TestCase
 
 
     agent = Mechanize.new
-    assert agent.get('http://www.joingrouper.com').get_assets == expected_assets
+    assets = agent.get('http://www.joingrouper.com').get_assets
+
+    assert assets == expected_assets, "#{assets} don't match expected_assets"
 
   end
 
@@ -87,7 +125,7 @@ class MechanizeCrawlerTest < Test::Unit::TestCase
     agent = Mechanize.new
     links = agent.get('http://joingrouper.com').get_links
 
-    assert links == expected_links
+    assert links == expected_links, "#{links.inspect} don't match expected_links"
   end
 
 end
